c6037663792d70a3ad65960e0d2d645aac5c9549
huy023156 <huy023156@gmail.com>
2025-04-22 21:08:54 +0700

    Basket domain models and auto migration

---
+++ src/Modules/Basket/Basket/Basket/Models/ShoppingCart.cs
@@ -0,0 +1,42 @@
+﻿namespace Basket.Basket.Models;
+
+public class ShoppingCart : Aggregate<Guid> {
+    public string UserName { get; private set; } = default!;
+
+    private readonly List<ShoppingCartItem> _items = new();
+    public IReadOnlyCollection<ShoppingCartItem> Items => _items.AsReadOnly();
+    public decimal TotalPrice => _items.Sum(x => x.Price * x.Quantity);
+
+    public static ShoppingCart Create(Guid id, string userName) {
+        ArgumentException.ThrowIfNullOrEmpty(userName);
+
+        var shoppingCart = new ShoppingCart {
+            Id = id,
+            UserName = userName
+        };
+
+        return shoppingCart;
+    }
+
+    public void AddItem(Guid productId, int quantity, string color, decimal price, string productName) {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(quantity);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+        var existingItem = Items.FirstOrDefault(x => x.ProductId == productId);
+
+        if (existingItem is not null) {
+            existingItem.Quantity += quantity;
+        } else {
+            existingItem = new ShoppingCartItem(Id, productId, quantity, color, price, productName);
+            _items.Add(existingItem);
+        }
+    }
+
+    public void RemoveItem(Guid productId) {
+        var item = Items.FirstOrDefault(x => x.ProductId == productId);
+
+        if (item is not null) {
+            _items.Remove(item);
+        }
+    }
+}
--- src/Modules/Basket/Basket/BasketModule.cs
+++ src/Modules/Basket/Basket/BasketModule.cs
@@ -1,13 +1,29 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Shared.Data.Interceptors;
 
 namespace Basket;
 public static class BasketModule {
     public static IServiceCollection AddBasketModule(this IServiceCollection services, IConfiguration configuration) {
         // Add services to the container.
-        // services.AddScoped<IBasketService, BasketService>();
-        // services.AddScoped<IBasketRepository, BasketRepository>();
+
+        // Api Endpoint services
+
+        // Application Use Case services
+
+        // Data - Infrastructure services
+        var connectionString = configuration.GetConnectionString("Database");
+
+        services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+        services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
+        services.AddDbContext<BasketDbContext>((sp, options) => {
+            options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+            options.UseNpgsql(connectionString);
+        });
+
         return services;
     }
 
